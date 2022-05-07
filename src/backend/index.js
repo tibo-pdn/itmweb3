1d43d848d5e82c35794be1ccd5d43bf5f32fe4e7
Tibo Pendino <tibo.pendino@epitech.eu>
2022-05-07 07:00:24 +0200

    [ADD] : / route on API and route file with helmet security and dotenv variables set up

--- src/backend/index.js
+++ src/backend/index.js
@@ -6,9 +6,36 @@
  */
 
 const express = require('express');
+const dotenv = require('dotenv');
 const https = require('https');
 const axios = require('axios');
-const dotenv = require('dotenv');
+const helmet = require('helmet');
 const app = express();
+// const router = require('./routes')
 
-dotenv.config();
+dotenv.config({ path: './config/.env' });
+
+const STARTON_API_KEY = process.env.STARTON_API_KEY;
+const HOST = process.env.HOST;
+const PORT = process.env.PORT;
+
+app.use(helmet());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.get('/', ((req, res) => {
+    res.status(200).send({
+        date: new Date(),
+        status: "Up"
+    });
+}));
+
+app.listen(PORT, HOST, (err) => {
+    if (err) {
+        console.error(err);
+        throw err;
+    } else {
+        console.info(`Starton Hackaton Project API is listening on: ${HOST}:${PORT}`);
+    }
+})
---
+++ src/backend/routes/routes.js
@@ -0,0 +1,6 @@
+/**
+ * THis file is a part of Starton Hackaton Projet
+ * 
+ * @author Tibo Pendino
+ * @copyright 2022 - Starton Hackaton Projet. All rights reserved
+ */
