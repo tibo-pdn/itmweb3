c3e16246bbf37c921f3b606c25976b1f526d1f51
Tibo Pendino <tibo.pendino@epitech.eu>
2022-05-07 06:12:14 +0200

    [ADD] : backend and frontend folders. Index.js and some package includes for backend

---
+++ src/backend/index.js
@@ -0,0 +1,14 @@
+/**
+ * THis file is a part of Starton Hackaton Projet
+ * 
+ * @author Tibo Pendino
+ * @copyright 2022 - Starton Hackaton Projet. All rights reserved
+ */
+
+const express = require('express');
+const https = require('https');
+const axios = require('axios');
+const dotenv = require('dotenv');
+const app = express();
+
+dotenv.config();
